a6d8b0260db7fc04777f6f4a948c7230638c39b7
Tanisha Maurya <12tanishamaurya@gmail.com>
2026-07-14 22:06:06 +0530

    Add url in cors

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -17,7 +17,7 @@ const required = (key) => {
 export const env = {
   port: process.env.PORT || 5000,
   nodeEnv: process.env.NODE_ENV || 'development',
-  clientUrl: process.env.CLIENT_URL || 'http://localhost:4200',
+  clientUrl: 'https://vision-craft-ai-tau.vercel.app' || process.env.CLIENT_URL,
 
   jwtSecret: required('JWT_SECRET'),
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
